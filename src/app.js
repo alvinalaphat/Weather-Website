a9fba37e372cd89b391c302e544a10bda469e16c
Alvin Alaphat <alvinalaphat@gmail.com>
2019-08-09 17:10:25 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,87 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast') 
+
+const app = express()
+
+// Define paths
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Set up handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Set up static directory to use
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Alvin Alaphat'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Alvin Alaphat'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Alvin Alaphat',
+        message: 'If you need help, just ask.'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    } 
+    
+    geocode(req.query.address, (error, {longitude,latitude,location} = {}) => {
+        if (error) {
+            return console.log(error)
+        } 
+
+        forecast(longitude, latitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            res.send({
+                location,
+                forecastData,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        message: 'Help article not found',
+        name: 'Alvin Alaphat'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        message: 'Page not found',
+        name: 'Alvin Alaphat'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (longitude, latitude, callback) => {
+    url = 'https://api.darksky.net/forecast/296d89b54641b747cbc795319bcb3ba3/'+ latitude + ',' + longitude
+
+    request({ url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather service', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
